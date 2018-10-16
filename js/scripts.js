79853850c8eb23e4fb0c2c5af10b5ba411d048a6
Scott Bergler <commandinghands@gmail.com>
2018-10-15 22:22:18 -0700

    add jQuery functions and event listeners on html elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,58 @@
-var userMessage = prompt("Say something!");
-console.log(userMessage);
+$(function() {
+  var userMessage = prompt("Say something!");
+  console.log(userMessage);
 
-var capitalizeFirstLast = function(input) {
-  var firstLast = input.charAt(0) + input.charAt(input.length-1);
-  console.log(firstLast);
+  var capitalizeFirstLast = function(input) {
+    var firstLast = input.charAt(0) + input.charAt(input.length-1);
+    console.log(firstLast);
 
-  var firstLastInCaps = firstLast.toUpperCase();
-  console.log(firstLastInCaps);
+    var firstLastInCaps = firstLast.toUpperCase();
+    console.log(firstLastInCaps);
 
-  return firstLastInCaps;
-};
+    return firstLastInCaps;
+  };
 
-var reverseFirstLast = function(letterPair) {
-  var lastFirst = letterPair.charAt(1) + letterPair.charAt(0);
+  var reverseFirstLast = function(letterPair) {
+    var lastFirst = letterPair.charAt(1) + letterPair.charAt(0);
 
-  console.log(lastFirst);
+    console.log(lastFirst);
 
-  return lastFirst;
-};
+    return lastFirst;
+  };
 
-var joinStrings = function(userMessage) {
-  var letterPair = capitalizeFirstLast(userMessage);
+  var joinStrings = function(userMessage) {
+    var letterPair = capitalizeFirstLast(userMessage);
 
-  var joinedStrings = userMessage + reverseFirstLast(letterPair);
-  console.log(joinedStrings);
-  return joinedStrings;
-};
+    var joinedStrings = userMessage + reverseFirstLast(letterPair);
+    console.log(joinedStrings);
+    return joinedStrings;
+  };
 
-var final = function(four) {
-  var middleLetter= four.charAt(parseInt(four.length/2));
+  // var final = function(userMessage) {
+  //   var userMessage = prompt("Say something!");
+  //   console.log(userMessage);
+  //   var middleLetter = userMessage.charAt(parseInt(userMessage.length/2));
+  //   var original = joinStrings(userMessage);
+  //   var joined = middleLetter + original;
+  //   console.log(joined);
+  //
+  //   document.getElementById("processedInput").textContent = joined;
+  //   // return joined;
+  // };
+  
+  $("#sweet").click(function() {
+    alert(userMessage);
+  });
 
-  console.log(middleLetter + joinStrings(four));
+  $("#angry").click(function() {
 
-  return middleLetter;
-};
+    var middleLetter = userMessage.charAt(parseInt(userMessage.length/2));
+    var original = joinStrings(userMessage);
+    var joined = middleLetter + original;
+    console.log(joined);
 
-final(userMessage);
+    document.getElementById("processedInput").textContent = joined;
+
+  });
+
+});
